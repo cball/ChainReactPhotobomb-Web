c32d809da1fbdd0fbb0310e2e7d7dcb1df5ae7e8
Chris Ball <chris@echobind.com>
2017-06-30 12:52:14 -0400

    Slight style updates

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import './App.css';
 import { Link, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Privacy from './Components/Privacy';
+import logo from './Images/logo.svg';
+import appStoreBadge from './Images/app-store.svg';
+import playStoreBadge from './Images/play-store.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/privacy">Privacy</Link>
-          </nav>
+      <div className="App container">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+          <h2>Photobomb!</h2>
+        </div>
+        <div className="badges">
+          <a href="http://play.google.com/store/chainreactphotobomb?pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+            <img alt="Get it on Google Play" src={playStoreBadge} />
+          </a>
+          <a href="http://google.com">
+            <img alt="Get it on the App Store" src={appStoreBadge} />
+          </a>
         </div>
 
         <div>
           <Route exact path="/" component={Home} />
           <Route path="/privacy" component={Privacy} />
         </div>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/privacy">Privacy</Link>
+        </nav>
       </div>
     );
   }
