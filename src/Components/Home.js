94bc2c9bc461629330150e71d43f509145645c79
Chris Ball <chris@echobind.com>
2017-06-30 13:42:17 -0400

    copy paste apollo from RN app

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,109 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { gql, graphql } from 'react-apollo';
 
-export default () => {
-  return (
-    <div>
-      <h1>Will be cool home page.</h1>
-    </div>
-  );
+export const allPhotosQuery = gql`
+  query {
+    allPhotos(orderBy: createdAt_DESC) {
+      id
+      file {
+        id
+        url
+      }
+    }
+  }
+`;
+
+const photosSubscription = gql`
+  subscription createPhoto {
+    Photo(filter: { mutation_in: [CREATED] }) {
+      mutation
+      node {
+        id
+        file {
+          id
+          url
+        }
+      }
+    }
+  }
+`;
+
+const PHOTO_SIZE = 96;
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.subscription = null;
+  }
+  componentWillReceiveProps(nextProps) {
+    this._subscribeToNewPhotos(nextProps);
+  }
+  renderLoading() {
+    return <div>Loading!</div>;
+  }
+  renderPhotoList = () => {
+    const { allPhotos } = this.props.allPhotosQuery;
+    debugger;
+
+    return allPhotos.map(p =>
+      <div>
+        {this.renderPhoto(p)}
+      </div>
+    );
+  };
+
+  renderPhoto = photo => {
+    const { id, file } = photo;
+    const imagePath = file.url.replace('files', 'images');
+    // get smallest size possible accounting for 2x dpi
+    const roundedSize = Math.ceil(PHOTO_SIZE) * 2;
+    const uri = `${imagePath}/${roundedSize}x${roundedSize}`;
+
+    return <img src={uri} />;
+  };
+
+  render() {
+    const { loading, error } = this.props.allPhotosQuery;
+    const shouldRenderLoading = !error && loading;
+    const shouldRenderPhotoList = !error && !shouldRenderLoading;
+
+    return (
+      <div>
+        {error && <div>Error fetching :(</div>}
+        {shouldRenderLoading && this.renderLoading()}
+        {shouldRenderPhotoList && this.renderPhotoList()}
+      </div>
+    );
+  }
+
+  _subscribeToNewPhotos(nextProps) {
+    this._unsubscribeIfPropsChanged(nextProps);
+
+    if (!this.subscription && !nextProps.allPhotosQuery.loading) {
+      this.subscription = nextProps.allPhotosQuery.subscribeToMore({
+        document: photosSubscription,
+        updateQuery: prependNewPhotos
+      });
+    }
+  }
+
+  _unsubscribeIfPropsChanged(nextProps) {
+    if (!this.subscription) return;
+
+    if (this.props.subscriptionParam !== nextProps.subscriptionParam) {
+      this.subscription.unsubscribe();
+      this.subscription = null;
+    }
+  }
+}
+
+const prependNewPhotos = (previousState, { subscriptionData }) => {
+  const newPhoto = subscriptionData.data.Photo.node;
+  const allPhotos = [newPhoto, ...previousState.allPhotos];
+
+  return {
+    allPhotos
+  };
 };
+
+export default graphql(allPhotosQuery, { name: 'allPhotosQuery' })(Home);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface
+} from 'react-apollo';
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from 'subscriptions-transport-ws';
+
+const GRAPHQL_ENDPOINT =
+  'https://api.graph.cool/simple/v1/cj3g3v2hp18ag01621354vr2y';
+const GRAPHQL_WEBSOCKET_ENDPOINT =
+  'wss://subscriptions.graph.cool/v1/cj3g3v2hp18ag01621354vr2y';
+
+const networkInterface = createNetworkInterface({ uri: GRAPHQL_ENDPOINT });
+const wsClient = new SubscriptionClient(GRAPHQL_WEBSOCKET_ENDPOINT, {
+  reconnect: true
+});
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+);
+const client = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions
+});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 registerServiceWorker();
